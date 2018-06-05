383c60a7d13f9fb454fbd5bf75d9cd2cbcc9d46b
gzxu <37564520+gzxu@users.noreply.github.com>
2018-06-05 15:52:38 +0800

    Ignore antivirus errors on file downloading

--- onedrive/sdk.py
+++ onedrive/sdk.py
@@ -141,7 +141,7 @@ def download_file(session: Session, identifier: str, path: Union[str, Path], *, 
     path = Path(path)
     algorithms = {algorithm: algorithm() for algorithm in checksum}
     with path.open('wb') as file:
-        response = session.get(MSGRAPH_ENDPOINT + '/me/drive/items/' + identifier + '/content', stream=True)
+        response = session.get(MSGRAPH_ENDPOINT + '/me/drive/items/' + identifier + '/content?AVOverride=1', stream=True)
         response.raise_for_status()
         for chunk in response.iter_content():
             file.write(chunk)
